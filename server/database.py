3acbb26911527520d217d5c812a07975adcf14a6
hojun lee <leehojun.full@gmail.com>
2025-05-13 22:11:34 +0900

    클라이언트와 서버와의 통신 - 응답 및 답변 db 저장 기초 구현

---
+++ server/database.py
@@ -0,0 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class UserInteraction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_input = db.Column(db.Text, nullable=False)
+    server_response = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<Interaction(input='{self.user_input}', response='{self.server_response}')>"
+
+def init_db(app):
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
